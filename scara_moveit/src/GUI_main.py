38b5a74c4e12bad8ff8c57f27ad8b3a225d1bfbc
Patryk Parylo <pparylo@gmail.com>
2021-05-20 22:50:07 +0200

    Add GUI functionalities for joints movement.

--- scara_moveit/src/GUI_main.py
+++ scara_moveit/src/GUI_main.py
@@ -8,6 +8,7 @@ import moveit_commander
 from geometry_msgs.msg import Pose
 
 import time
+import math
 
 class MainWindow(QtWidgets.QMainWindow):
 
@@ -21,28 +22,68 @@ class MainWindow(QtWidgets.QMainWindow):
 		self.scene = moveit_commander.PlanningSceneInterface()
 		self.group = moveit_commander.MoveGroupCommander('scara_arm')
 
-		# Button
-		self.move_button.clicked.connect(self.move_to)
+		# Buttons
+		self.move_button.clicked.connect(self.move_to_XYZ)
 
-	def update_XYZ_position(self):
+		# Sliders
+		self.slider_pressed_flag = False
+
+		self.slider_slider.sliderPressed.connect(self.slider_pressed)
+		self.slider_slider.sliderReleased.connect(lambda: self.move_to_joint('slider', True))
+		
+		self.rev1_slider.sliderPressed.connect(self.slider_pressed)
+		self.rev1_slider.sliderReleased.connect(lambda: self.move_to_joint('rev1', True))
+		
+		self.rev2_slider.sliderPressed.connect(self.slider_pressed)
+		self.rev2_slider.sliderReleased.connect(lambda: self.move_to_joint('rev2', True))
+
+	def slider_pressed(self):
+		self.slider_pressed_flag = True
+
+
+	def update_robot_state_labels(self):
 		'''
-		Get current XYZ position of end-effector from Moveit Commander. Convert values from [m] to [mm] and display then in corresponding fields in GUI.
+		Get current XYZ position of end-effector and joints position from Moveit Commander. 
+		Convert values from [m] to [mm] and from radians to degrees. 
+		Display values in corresponding fields on GUI.
 		'''
 
-		positions = self.group.get_current_pose().pose.position
+		xyz_position = self.group.get_current_pose().pose.position
+		joints_position = self.group.get_current_joint_values()
 
-		X_position_text = str(round(positions.x*1000))
-		Y_position_text = str(round(positions.y*1000))
-		Z_position_text = str(round(positions.z*1000))
+		x_position = round(xyz_position.x*1000)
+		y_position = round(xyz_position.y*1000)
+		z_position = round(xyz_position.z*1000)
 
-		self.X_current_pos.setText(X_position_text)
-		self.Y_current_pos.setText(Y_position_text)
-		self.Z_current_pos.setText(Z_position_text)
+		slider_current = round(joints_position[0]*1000)
+		rev1_current = round(math.degrees(joints_position[1]))
+		rev2_current = round(math.degrees(joints_position[2]))
 
-	def move_to(self):
+		self.x_current_pos.setText(str(x_position))
+		self.y_current_pos.setText(str(y_position))
+		self.z_current_pos.setText(str(z_position))
+
+		if self.slider_pressed_flag:
+			self.slider_current_val.setText(str(self.slider_slider.value()))
+			self.rev1_current_val.setText(str(self.rev1_slider.value()))
+			self.rev2_current_val.setText(str(self.rev2_slider.value()))
+		else:	
+			self.slider_current_val.setText(str(slider_current))
+			self.rev1_current_val.setText(str(rev1_current))
+			self.rev2_current_val.setText(str(rev2_current))
+		
+		
+		if not self.slider_pressed_flag:
+			self.slider_slider.setValue(slider_current)
+			self.rev1_slider.setValue(rev1_current)
+			self.rev2_slider.setValue(rev2_current)
+		
+	def move_to_XYZ(self):
 		'''
-		Get end-efector XYZ possition from user. Send mentioned position to Moveit Commander in order to execute manipulator motion. 
+		Get end-efector XYZ possition from user. 
+		Send position to Moveit Commander in order to execute manipulator motion. 
 		'''
+
 		target_pose = Pose()
 
 		target_pose.position.x = int(self.X_line_edit.text())/1000
@@ -53,6 +94,29 @@ class MainWindow(QtWidgets.QMainWindow):
 		self.group.set_position_target([target_pose.position.x,  target_pose.position.y, target_pose.position.z], 'gripper')
 		self.group.set_goal_tolerance(0.005)
 		self.group.go()
+
+	def move_to_joint(self, joint, slider_used = False):
+		'''
+		Get joint positions from slider possition.
+		Send joint position to Moveit Commander in order to execute manipulator motion. 
+		'''
+		if slider_used:
+			self.slider_pressed_flag = False
+
+		if joint == 'slider':
+			joint_pos_target = self.slider_slider.value()/1000
+			self.group.set_joint_value_target([joint_pos_target, 0, 0])
+		elif joint == 'rev1':
+			joint_pos_target  = math.radians(self.rev1_slider.value())
+			self.group.set_joint_value_target([0.2, joint_pos_target, 0])
+		elif joint == 'rev2':
+			joint_pos_target  = math.radians(self.rev2_slider.value())
+			self.group.set_joint_value_target([0.2, 0, joint_pos_target,])
+		
+		
+		self.group.go()
+
+		
 
 
 
@@ -67,7 +131,7 @@ if __name__ == '__main__':
 	widget.show()
 
 	timer = QtCore.QTimer()
-	timer.timeout.connect(widget.update_XYZ_position)
+	timer.timeout.connect(widget.update_robot_state_labels)
 	timer.start(200) 
 
 	sys.exit(app.exec_())
