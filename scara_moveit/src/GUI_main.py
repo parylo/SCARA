156a682bf35e317df49cacff04602708c2714a59
Patryk Parylo <pparylo@gmail.com>
2021-05-22 15:31:05 +0200

    Add GUI functionalities for moving manipulator in XYZ axis with predfined distance from radion buttons. Add home button action.

--- scara_moveit/src/GUI_main.py
+++ scara_moveit/src/GUI_main.py
@@ -16,7 +16,6 @@ class MainWindow(QtWidgets.QMainWindow):
 		super().__init__()
 		loadUi('QtDesigner_GUI.ui', self)
 
-
 		# Moveit commander setup
 		self.robot = moveit_commander.RobotCommander()
 		self.scene = moveit_commander.PlanningSceneInterface()
@@ -24,44 +23,75 @@ class MainWindow(QtWidgets.QMainWindow):
 
 		# Buttons
 		self.move_button.clicked.connect(self.move_to_XYZ)
+		self.home_button.clicked.connect(self.home_action)
+		# self.gripper_button.clicked.connect(self.gripper_action)
+		
 
+		self.xplus_button.clicked.connect(lambda: self.move_to_direction('x'))
+		self.xminus_button.clicked.connect(lambda: self.move_to_direction('x', False))
+		self.yplus_button.clicked.connect(lambda: self.move_to_direction('y'))
+		self.yminus_button.clicked.connect(lambda: self.move_to_direction('y', False))
+		self.zplus_button.clicked.connect(lambda: self.move_to_direction('z'))
+		self.zminus_button.clicked.connect(lambda: self.move_to_direction('z', False))
+
+		# Radio Buttons
+		self.move_distance = 0.01
+		self.radio_button_1.toggled.connect(lambda:self.set_distance(self.radio_button_1))
+		self.radio_button_2.toggled.connect(lambda:self.set_distance(self.radio_button_2))
+		self.radio_button_3.toggled.connect(lambda:self.set_distance(self.radio_button_3))
+	
 		# Sliders
 		self.slider_pressed_flag = False
 
 		self.slider_slider.sliderPressed.connect(self.slider_pressed)
 		self.slider_slider.sliderReleased.connect(lambda: self.move_to_joint('slider', True))
-		
 		self.rev1_slider.sliderPressed.connect(self.slider_pressed)
 		self.rev1_slider.sliderReleased.connect(lambda: self.move_to_joint('rev1', True))
-		
 		self.rev2_slider.sliderPressed.connect(self.slider_pressed)
 		self.rev2_slider.sliderReleased.connect(lambda: self.move_to_joint('rev2', True))
 
 	def slider_pressed(self):
+		'''Update slider pressed flag.'''
+
 		self.slider_pressed_flag = True
 
+	def set_distance(self, radio_button):
+		'''After pressing radio button, distance to move is set value from button label (convert [mm] -> [m]).'''
 
-	def update_robot_state_labels(self):
+		if radio_button.isChecked():
+			button_value = int(radio_button.text().split()[0])
+			self.move_distance = button_value/1000
+
+	def update_XYZ_labels(self):
 		'''
-		Get current XYZ position of end-effector and joints position from Moveit Commander. 
-		Convert values from [m] to [mm] and from radians to degrees. 
+		Get current XYZ position of end-effector from Moveit Commander. 
+		Convert [m] -> [mm]. 
 		Display values in corresponding fields on GUI.
 		'''
 
 		xyz_position = self.group.get_current_pose().pose.position
-		joints_position = self.group.get_current_joint_values()
-
+	
 		x_position = round(xyz_position.x*1000)
 		y_position = round(xyz_position.y*1000)
 		z_position = round(xyz_position.z*1000)
 
-		slider_current = round(joints_position[0]*1000)
-		rev1_current = round(math.degrees(joints_position[1]))
-		rev2_current = round(math.degrees(joints_position[2]))
-
 		self.x_current_pos.setText(str(x_position))
 		self.y_current_pos.setText(str(y_position))
 		self.z_current_pos.setText(str(z_position))
+
+	def update_joints_position_labels(self):
+		'''
+		Get current joints position from Moveit Commander. 
+		Convert [m] -> [mm] and [rad] -> [deg]. 
+		Display values in corresponding fields on GUI. Update joints slider position.
+		In case when user grab a slider, labels values are updated from slider position.
+		'''
+
+		joints_position = self.group.get_current_joint_values()
+
+		slider_current = round(joints_position[0]*1000)
+		rev1_current = round(math.degrees(joints_position[1]))
+		rev2_current = round(math.degrees(joints_position[2]))
 
 		if self.slider_pressed_flag:
 			self.slider_current_val.setText(str(self.slider_slider.value()))
@@ -90,7 +120,6 @@ class MainWindow(QtWidgets.QMainWindow):
 		target_pose.position.y = int(self.Y_line_edit.text())/1000
 		target_pose.position.z = int(self.Z_line_edit.text())/1000
 
-
 		self.group.set_position_target([target_pose.position.x,  target_pose.position.y, target_pose.position.z], 'gripper')
 		self.group.set_goal_tolerance(0.005)
 		self.group.go()
@@ -100,25 +129,65 @@ class MainWindow(QtWidgets.QMainWindow):
 		Get joint positions from slider possition.
 		Send joint position to Moveit Commander in order to execute manipulator motion. 
 		'''
+
+		joints_position = self.group.get_current_joint_values()
+
 		if slider_used:
 			self.slider_pressed_flag = False
 
 		if joint == 'slider':
 			joint_pos_target = self.slider_slider.value()/1000
-			self.group.set_joint_value_target([joint_pos_target, 0, 0])
+			joints_position[0] = joint_pos_target
+			self.group.set_joint_value_target(joints_position)
+		
 		elif joint == 'rev1':
 			joint_pos_target  = math.radians(self.rev1_slider.value())
-			self.group.set_joint_value_target([0.2, joint_pos_target, 0])
+			joints_position[1] = joint_pos_target
+			self.group.set_joint_value_target(joints_position)
+		
 		elif joint == 'rev2':
 			joint_pos_target  = math.radians(self.rev2_slider.value())
-			self.group.set_joint_value_target([0.2, 0, joint_pos_target,])
-		
+			joints_position[2] = joint_pos_target
+			self.group.set_joint_value_target(joints_position)
 		
 		self.group.go()
 
+	def move_to_direction(self, axis, dir_positive = True):
+		'''Move end-effector to specific direction. Distance to move is based on user choice of radio buttons.'''
+
+		target_position = self.group.get_current_pose().pose.position
+
+		if axis == 'x':
+			if dir_positive:
+				target_position.x += self.move_distance
+			else:
+				target_position.x -= self.move_distance
+		elif axis == 'y':
+			if dir_positive:
+				target_position.y += self.move_distance
+			else:
+				target_position.y -= self.move_distance
+		elif axis == 'z':
+			if dir_positive:
+				target_position.z += self.move_distance
+			else:
+				target_position.z -= self.move_distance
+
+		self.group.set_position_target([target_position.x,  target_position.y, target_position.z], 'gripper')
+
+		self.group.set_goal_tolerance(0.005)
+		self.group.go()
 		
+	def home_action(self):
+		'''Move manipilator to predefined home position.'''
 
+		self.group.set_named_target("home")
+		self.group.go()
 
+	# def gripper_action(self):
+
+		#TO DO
+		
 
 if __name__ == '__main__':
 
@@ -131,7 +200,8 @@ if __name__ == '__main__':
 	widget.show()
 
 	timer = QtCore.QTimer()
-	timer.timeout.connect(widget.update_robot_state_labels)
+	timer.timeout.connect(widget.update_XYZ_labels)
+	timer.timeout.connect(widget.update_joints_position_labels)
 	timer.start(200) 
 
 	sys.exit(app.exec_())
